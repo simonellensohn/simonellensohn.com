149faa240e5eb00985d81792dc8b07316a30170e
Simon Ellensohn <ellensohn.simon@gmail.com>
2026-01-21 12:13:26 +0100

    feat: generate new og-image

--- app/app.vue
+++ app/app.vue
@@ -19,8 +19,8 @@ useHead({
 
 useSeoMeta({
   titleTemplate: '%s - Simon Ellensohn',
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png',
+  ogImage: '/og-image.png',
+  twitterImage: '/og-image.png',
   twitterCard: 'summary_large_image',
 })
 </script>
