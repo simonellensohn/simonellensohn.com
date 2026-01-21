e78383e0da1cc1360edc30cfdf8ff49fcd496116
Simon Ellensohn <ellensohn.simon@gmail.com>
2026-01-21 12:13:35 +0100

    feat: use emoji as favicon

--- app/app.vue
+++ app/app.vue
@@ -11,7 +11,7 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     { key: 'theme-color', name: 'theme-color', content: color },
   ],
-  link: [{ rel: 'icon', href: '/favicon.ico' }],
+  link: [{ rel: 'icon', href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>' }],
   htmlAttrs: {
     lang: 'en',
   },
